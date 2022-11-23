32ee6a02fc79577c90eef6745f6bce652cb9c6ab
DavinciCode <emsthias33@gmail.com>
2022-11-23 01:07:34 +0100

    done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -429,10 +429,10 @@ export const Home = () => {
 								/>
 							</div>
 							<button className='text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
-								Button
+								Submit
 							</button>
 							<p className='text-xs text-gray-500 mt-3'>
-								Literally you probably haven't heard of them jean shorts.
+								Get amazing offers in your inbox
 							</p>
 						</div>
 					</div>
